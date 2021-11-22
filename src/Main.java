79c0fabecea138f02a7b805a932e883090fab0a2
Nonopichy <68911691+Nonopichy@users.noreply.github.com>
2021-11-22 11:45:57 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,13 @@ import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 import java.util.concurrent.TimeUnit;
 
-public class Main extends
-        Canvas implements Runnable, MouseListener {
-    public static int button = 1;
-    public static int por = 1;
-    public static int last_click = 0;
-    public static final int width = 150, height = 250;
+public class Main extends Canvas implements Runnable, MouseListener {
+    public static final int width = 150, height = 250, click = 0, batter = 0, button = 1, por = 1, last_click = 0;
     public Main(){
         Dimension d = new Dimension(width,height);
         this.setPreferredSize(d);
         addMouseListener(this);
     }
-
-    public static int click = 0;
-    public static int batter = 0;
     public static void main(String args[]){
         Main main = new Main();
         JFrame jframe = new JFrame("CPS");
@@ -27,18 +20,6 @@ public class Main extends
         jframe.pack();
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setVisible(true);
-
-      //  JButton button = new JButton("Click Here..!");
-       // button.setBounds(width/2,height/2,1,2); /*Distance from left,
-        //              Distance from top,length of button, height of button*/
-       // jframe.add(button);
-
-
-      //  JButton b = new JButton("Button");
-      //  jframe.setLayout(null);
-      //  jframe.add(b);
-     //   b.setSize(400,400);
-       // b.setVisible(true);
 
         new Thread(main).start();
         new Thread(new e()).start();
@@ -49,41 +30,47 @@ public class Main extends
             this.createBufferStrategy(3);
             return;
         }
-        Graphics g = bs.getDrawGraphics();
-        g.setColor(new Color(240, 248, 255));
+        final Graphics g = bs.getDrawGraphics();
+        draw(g);
+        bs.show();
+    }
+    private final Color a = new Color(240, 248, 255);
+    private final Color b = new Color(107, 124, 130);
+    private final Color c = new Color(100, 100, 100);
+    public void draw(final Graphics g){
+        //CPS
+        g.setColor(a);
         g.fillRect(0, 0, width, height);
-        g.setColor(new Color(107, 124, 130));
+        g.setColor(b);
         g.setFont(new Font("Arial", Font.PLAIN, 23));
         g.drawString("CPS: " + click, width / 2 - 40, 20);
-        //
-        g.setColor(new Color(107, 124, 130));
+        //LAST
+        g.setColor(b);
         g.setFont(new Font("Arial", Font.PLAIN, 23));
         g.drawString("Last: " + last_click, width / 2 - 40, 40);
-        //
-        g.setColor(new Color(107, 124, 130));
+        //BATTER
+        g.setColor(b);
         g.setFont(new Font("Arial", Font.PLAIN, 23));
         g.drawString("Batter: " + batter, width / 2 - 40, 60);
-        //
+        //BUTTON
         g.setColor(Color.RED);
         g.fillRect(0, 230, 30, 20);
-        g.setColor(new Color(100, 100, 100));
+        g.setColor(c);
         g.setFont(new Font("Arial", Font.PLAIN, 12));
         if(button==1){
             g.drawString("Left", 0, 245);
-        } else if (button == 3){
+        else 
             g.drawString("Right", 0, 245);
-        }
+        //CONTADOR
         g.setColor(Color.GREEN);
         g.fillRect(120, 230, 30, 20);
-        g.setColor(new Color(100, 100, 100));
+        g.setColor(c);
         g.setFont(new Font("Arial", Font.PLAIN, 12));
         g.drawString("Por: "+por, 120, 245);
-
-        g.setColor(new Color(100, 100, 100));
+        //SUBTITLE
+        g.setColor(c);
         g.setFont(new Font("Arial", Font.PLAIN, 12));
         g.drawString("By NoRois", 45, 245);
-
-        bs.show();
     }
     public void update(){
         if(click>batter)batter=click;
@@ -92,26 +79,14 @@ public class Main extends
         while(true){
             render();
             update();
-            try{
-                Thread.sleep(1000/60);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            try{ Thread.sleep(1000/60); } 
+                catch (InterruptedException e) {e.printStackTrace();}
         }
     }
     public Boolean isWithin(int x, int y, int x1, int x2, int y1, int y2){
-        if(x >= x1 && x <= x2) {
-            if (y <= y1 && y >= y2) {
-                return true;
-            }
-        }
-        return false;
+        return(x >= x1 && x <= x2 && y <= y1 && y >= y2) 
     }
-
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
+    public void mouseClicked(MouseEvent e) {}
     public void mousePressed(MouseEvent e) {
         if(isWithin(e.getX(), e.getY(), 121, 149, 248, 229)) {
             if(e.getButton()==1){
@@ -132,18 +107,8 @@ public class Main extends
         }
         if (e.getButton() == button)
             click++;
-
     }
-
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    public void mouseExited(MouseEvent e) {
-
-    }
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
 }
